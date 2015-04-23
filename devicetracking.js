9790d042ec4094567970508a4af09d4a4f7597f9
gremlinc5 <chiesamichele84@gmail.com>
2015-04-23 11:09:37 +0200

    First commit

---
+++ devicetracking.js
@@ -0,0 +1,23 @@
+$(document).bind('deviceready', function () {
+	var watchId = 0;
+	
+	$('#btnWatch').bind('touchstart', function () {
+		If (watchId == 0) {
+			watchId = navigator.accelerometer.watchAcceleration( function(acceleration) {
+				$('#txtx').attr('value', acceleration.x);
+				$('#txty').attr('value', acceleration.y);
+				$('#txtz').attr('value', acceleration.z);
+			}, function (error) {
+				alert("Error reading accelerometer");
+			}, {
+				frequency: 100
+			});
+			
+			$('#btnWatch').html('Stop Watching');
+		} else {
+			navigator.accelerometer.clearWatch(watchId);
+			$('#btnWatch').html('Start Watching');
+			watchId = 0;
+		}
+	});
+});
